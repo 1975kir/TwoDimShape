3ee2e7d7c1fb9807f7e5e5e83c7eec8c44be6b37
1975kir <km1975@yandex.ru>
2022-02-26 22:28:53 +0300

    fifth commit

--- src/TwoDimShape/Shapes2.java
+++ src/TwoDimShape/Shapes2.java
@@ -4,6 +4,7 @@ public class Shapes2 {
     public static void main(String[] args) {
         Triangle t1 = new Triangle();
         Triangle t2 = new Triangle();
+        Square t3 = new Square();
 
         t1.setWidth (4.0);
         t1.setHeight (4.0);
@@ -12,6 +13,10 @@ public class Shapes2 {
         t2.setWidth (8.0);
         t2.setHeight (12.0);
         t2.style = "контурный";
+
+        t3.setWidth (5.0);
+        t3.setHeight (5.0);
+        t3.style = "в полоску";
 
         System.out.println("Информация о t1: ");
         t1.showStyle();
@@ -25,6 +30,12 @@ public class Shapes2 {
         t2.showDim();
         System.out.println("Площадь - " + t2.area());
 
+        System.out.println();
+
+        System.out.println("Информация о t3: ");
+        t3.showStyle();
+        t3.showDim();
+        System.out.println("Площадь - " + t3.area());
 
     }
 }
